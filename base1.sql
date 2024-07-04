841d074c099fcba468d11303dddee5e66f87e7af
papicoronel69 <cristofercoronel825@gmail.com>
2024-07-04 09:13:52 -0500

    BD2

--- base1.sql
+++ base1.sql
@@ -11,3 +11,8 @@ CREATE TABLE departamento(
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(50)
 );
+INSERT INTO empleados(nombre,departamento_id,salario)
+VALUES('Cristopher',1,1000.50),
+('Ikian',2,950.80),
+('Oscar',3,800.70);
+INSERT INTO departamento(nombre)VALUES('Contaduría'),('Gerencia'),('Limpieza');
